0e492f872f7f5441e8f28675fe9c9f5e161e7edd
Jerome Le Saux <jlesaux@free.fr>
2018-05-09 16:10:45 +0200

    add: add the switch bank when loading file.

--- makefile
+++ makefile
@@ -2,7 +2,7 @@
 DIR := $(shell echo */ | tr -d 'src/' )
 CSRC := $(shell find $(PHROZEN_PATH) -iname '*.c' | grep -v EXAMPLE )
 BIN := $(shell find . -print | grep bin)
-ASM := $(shell find . -print | grep .asm)
+ASM := $(shell find ./src/ -print | grep .asm)
 SCR := $(shell find . -print | grep .scr)
 PKG=iDSK
 default: 
@@ -21,8 +21,8 @@ checkc:
 package:
 	$(PKG) demo_src.dsk -n
 	@$(foreach b,$(SCR), $(PKG) demo_src.dsk -i $(b) -t 1;)
-	#@$(foreach c,$(CSRC), $(PKG) demo_src.dsk -i $(c) -t 0 -e C0000 -c 4000;)
-	$(PKG) demo_src.dsk -i src/demo.asm -t 0 -e C0000 -c 4000
+	@$(foreach c,$(ASM), $(PKG) demo_src.dsk -i $(c) -t 0 -e C0000 -c 4000;)
+	#$(PKG) demo_src.dsk -i src/demo.asm -t 0 -e C0000 -c 4000
 
 emulator:
 	javacpc "$(PWD)/demo_src.dsk"
--- src/demo.asm
+++ src/demo.asm
@@ -1,8 +1,8 @@
-  org #4000
+  org #3000
   nolist
 	write "demo"
 	jp main ; main function 
-
+;--------------------------------------------
 initamsdos: ; initialisation de la rom 7 (usage du disque)
 	ld hl,(#be7d); recup du numero de lecteur dans a
 	ld a,(hl)
@@ -14,25 +14,81 @@ initamsdos: ; initialisation de la rom 7 (usage du disque)
 	pop af
 	ld hl,(#be7d) ; restit du numero de lecteur
 	ld (hl),a
-
+  ret 
+;--------------------------------------------
 openfile: 
-	ld b,finhypto1-hypto1
-  ld hl,hypto1 ; nom du fichier a charger
-  ld de,#c000 ; buffer
   call #bc77
-  ld hl,#c000 ; adresse de dzbut du fichier
+  ld hl,#4000 ; adresse de debut du fichier
   call #bc83 ; read all content in memory
   call #bc7a ; close file 
+  ret	
+;--------------------------------------------
+
+cpyscreen:
+  ld hl,#4000 ;
+  ld de,#8000 ; c4 = bank 1 | c5 = bank 2 | c6 = bank 3 | c7 = bank 4
+  ld bc,#4000
+  ldir
+  ret	
 
 main: 
   call initamsdos
+
+  ; load and store file hypto1 in bank 1
+  ld bc,#7fc4
+  out (c),c
+  ld de,#8000 ; buffer
+  ld b,finhypto1-hypto1
+  ld hl,hypto1 ; nom du fichier a charger
   call openfile
+  call cpyscreen
+
+  ; ld a,2
+  ; call #bb5a
+  ; ; load and store file hypto2 in bank 2
+  ; ld bc,#7fc5
+  ; out (c),c
+  ; ld de,#8000 ; buffer
+  ; ld b,finhypto2-hypto2
+  ; ld hl,hypto2 ; nom du fichier a charger
+  ; call openfile
+  ; call cpyscreen
+
+
+  ; ld a,3
+  ; call #bb5a
+  ; ; load and store file hypto3 in bank 3
+  ; ld bc,#7fc6
+  ; out (c),c
+  ; ld de,#8000 ; buffer
+  ; ld b,finhypto3-hypto3
+  ; ld hl,hypto3 ; nom du fichier a charger
+  ; call openfile
+  ; call cpyscreen
+
+
+  ; ld a,4
+  ; call #bb5a
+  ; ; load and store file hypto4 in bank 4
+  ; ld bc,#7fc7
+  ; out (c),c
+  ; ld de,#8000 ; buffer
+  ; ld b,finhypto4-hypto4
+  ; ld hl,hypto4 ; nom du fichier a charger
+  ; call openfile
+  ; call cpyscreen
+
+  ld bc,#7fc0
+  out (c),c
+
+  call waitkey
+  ret	
 
 waitkey: 
   call #bb06
   ld h,0
   ld l,a
-  call #0 ; reset cpc
+  ret 
 
 hypto1: 
   db "hypto1.scr"
